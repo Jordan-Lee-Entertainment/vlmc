a79d65c25e9f33b7d58ea4ee4028fd5ed1e1a2b7
VideoLAN <videolan@videolan.org>
2017-09-19 13:36:52 +0200

    l10n: Bengali (India) update
    100% translated
    
    Signed-off-by: Michał Trzebiatowski <michtrz@gmail.com>

--- ts/vlmc_bn_IN.ts
+++ ts/vlmc_bn_IN.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="bn_IN" sourcelanguage="en" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bn_IN" sourcelanguage="en">
 <defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>AboutVLMC</name>
